72c5c65d16f41f5b83b7ff06935fed5911de7dbc
TheRog <rogerjschuck@gmail.com>
2015-12-31 10:20:03 -0800

    Working on a seperate joystick monitor object

--- LinuxProjectPi.cpp
+++ LinuxProjectPi.cpp
@@ -2,6 +2,9 @@
 #include<string.h>
 #include<stdio.h>
 #include<fcntl.h>
+#include <termios.h>		//Used for UART
+
+
 
 class LinuxFile
 {
@@ -105,6 +108,26 @@ public:
 	}
 }*/
 
+void MapJoystickToRelays(float joystick, bool &up_relay, bool &down_relay)
+{
+   bool temp_up = false;
+   bool temp_down = false;
+   if (joystick > 1000)
+   {
+      temp_up = true;
+   }
+   else if (joystick < -1000)
+   {
+      temp_down = true;
+   }
+   else
+   {
+      
+   }
+   up_relay = temp_up;
+   down_relay = temp_down;
+}
+
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -129,9 +152,71 @@ void write_to_screen() {
 	char *button = NULL, name_of_joystick[80];
 	struct js_event js;
 	time_t theTime;
+   
+   //-------------------------
+	//----- SETUP USART 0 -----
+	//-------------------------
+	//At bootup, pins 8 and 10 are already set to UART0_TXD, UART0_RXD (ie the alt0 function) respectively
+   int uart0_filestream = -1;
 	
+   	//OPEN THE UART
+   	//The flags (defined in fcntl.h):
+   	//	Access modes (use 1 of these):
+   	//		O_RDONLY - Open for reading only.
+   	//		O_RDWR - Open for reading and writing.
+   	//		O_WRONLY - Open for writing only.
+   	//
+   	//	O_NDELAY / O_NONBLOCK (same function) - Enables nonblocking mode. When set read requests on the file can return immediately with a failure status
+   	//											if there is no input immediately available (instead of blocking). Likewise, write requests can also return
+   	//											immediately with a failure status if the output can't be written immediately.
+   	//
+   	//	O_NOCTTY - When set and path identifies a terminal device, open() shall not cause the terminal device to become the controlling terminal for the process.
+   uart0_filestream = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY | O_NDELAY);		//Open in non blocking read/write mode
+   if (uart0_filestream == -1)
+   {
+   	//ERROR - CAN'T OPEN SERIAL PORT
+      printf("Error - Unable to open UART.  Ensure it is not in use by another application\n");
+   }
+	
+   	//CONFIGURE THE UART
+   	//The flags (defined in /usr/include/termios.h - see http://pubs.opengroup.org/onlinepubs/007908799/xsh/termios.h.html):
+   	//	Baud rate:- B1200, B2400, B4800, B9600, B19200, B38400, B57600, B115200, B230400, B460800, B500000, B576000, B921600, B1000000, B1152000, B1500000, B2000000, B2500000, B3000000, B3500000, B4000000
+   	//	CSIZE:- CS5, CS6, CS7, CS8
+   	//	CLOCAL - Ignore modem status lines
+   	//	CREAD - Enable receiver
+   	//	IGNPAR = Ignore characters with parity errors
+   	//	ICRNL - Map CR to NL on input (Use for ASCII comms where you want to auto correct end of line characters - don't use for bianry comms!)
+   	//	PARENB - Parity enable
+   	//	PARODD - Odd parity (else even)
+   struct termios options;
+   tcgetattr(uart0_filestream, &options);
+   options.c_cflag = B9600 | CS8 | CLOCAL | CREAD;		//<Set baud rate
+   options.c_iflag = IGNPAR;
+   options.c_oflag = 0;
+   options.c_lflag = 0;
+   tcflush(uart0_filestream, TCIFLUSH);
+   tcsetattr(uart0_filestream, TCSANOW, &options);
+   
+   //----- TX BYTES -----
+   unsigned char tx_buffer[20];
+   unsigned char *p_tx_buffer;
+	
+   p_tx_buffer = &tx_buffer[0];
+   *p_tx_buffer++ = 'H';
+   *p_tx_buffer++ = 'e';
+   *p_tx_buffer++ = 'l';
+   *p_tx_buffer++ = 'l';
+   *p_tx_buffer++ = 'o';
+	
+
 	LinuxGPIO gpio27(27);
 	gpio27.SetDirection(true);
+   
+   LinuxGPIO gpio17(17);
+   gpio17.SetDirection(true);
+   
+   bool arm1_up = false;
+   bool arm1_down = false;
 
 	wmove(stdscr, 8, 0);
 	addstr("Type \"q\" to quit.\n");
@@ -227,28 +312,41 @@ void write_to_screen() {
 		wmove(stdscr, 6, 17);
 		addstr(ctime(&theTime));
                       
-		wmove(stdscr, 12, 0);
+   	MapJoystickToRelays(-axis[1], arm1_up, arm1_down);
+   	gpio27.SetValue(arm1_up);
+   	gpio17.SetValue(arm1_down);
 		
-		bool led_state = false;
-		if (axis[1] > 100)
-		{			
-			addstr("Down");
-		}
-		else if (axis[1] < -100)
-		{
-			led_state = true;
-			wmove(stdscr, 12, 0);
-			addstr("Up");
-		}
-		else
-		{
-			addstr("      ");
-		}
-		
-		gpio27.SetValue(led_state);
+		//bool led_state = false;
+		//if (axis[1] > 100)
+		//{			
+			//addstr("Left Down      ");
+		//}
+		//else if (axis[1] < -100)
+		//{
+			//led_state = true;
+			//addstr("Left Up        ");
+		//}
+		//else
+		//{
+			//addstr("No Left Input ");
+		//}
+		//
+		//gpio27.SetValue(led_state);
+		//
+		//wmove(stdscr, 12, 0);
+		//addstr(led_state ? "GPIO 27 ON " : "GPIO 27 OFF");
 		
 		wmove(stdscr, 15, 0);
 		refresh();
+   	
+   	if (uart0_filestream != -1)
+   	{
+      	int count = write(uart0_filestream, &tx_buffer[0], (p_tx_buffer - &tx_buffer[0]));		//Filestream, bytes to write, number of bytes to write
+      	if (count < 0)
+      	{
+         	printf("UART TX error\n");
+      	}
+   	}
 	}
 }
 
